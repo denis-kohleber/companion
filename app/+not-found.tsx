a7f00d031580c0cb8f7593d514cb79bd95fbe7d1
Denis-K0 <160408077+Denis-K0@users.noreply.github.com>
2025-04-11 19:13:32 +0200

    Add not-found page

---
+++ app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+import { Text, View } from 'react-native-reanimated/lib/typescript/Animated';
+
+export default function NotFoundScreen() {
+    return (
+        <>
+            <Stack.Screen options={{ title: '404!' }} />
+            <View style={styles.container}>
+                <Text>Diese Seite existiert nicht.</Text>
+                <Link href="/" style={styles.link}>
+                    <Text>Zurück zum Start</Text>
+                </Link>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+});
