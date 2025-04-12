a6dfca42e15e45182a07330302e342572b8b517e
Denis-K0 <160408077+Denis-K0@users.noreply.github.com>
2025-04-12 17:01:43 +0200

    add "newPost" screen

--- app/newPost.tsx
+++ app/newPost.tsx
@@ -1,15 +1,82 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, TextInput, Button } from 'react-native';
 import React from 'react';
+import { Colors } from '@/utils/colors';
+import { useRouter } from 'expo-router';
 
-export default function newPost() {
+export default function NewPost() {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>WritePost</Text>
-            <Text>WritePost</Text>
-            <Text>WritePost</Text>
-            <Text>WritePost</Text>
-            <Text>WritePost</Text>
-            <Text>WritePost</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.main}>
+                <View style={styles.cardContainer}>
+                    <TextInput
+                        style={styles.titleInput}
+                        placeholder="Überschrift"
+                    />
+                    <TextInput
+                        multiline
+                        numberOfLines={5}
+                        style={styles.descriptionInput}
+                        placeholder="Wie war dein Tag?"
+                        textAlignVertical="top"
+                    />
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title="Speichern"
+                            onPress={() => console.log('Button betätigt.')}
+                            color={Colors.accent}
+                        />
+                        <Button
+                            title="Abbrechen"
+                            onPress={() => router.navigate('/(tabs)')}
+                            color={Colors.secondary3}
+                        />
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = {
+    main: {
+        padding: 15,
+        flex: 1,
+        gap: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row' as 'row',
+        justifyContent: 'space-around' as 'space-around',
+        marginTop: 20,
+    },
+    button: {
+        borderRadius: 20,
+    },
+    cardContainer: {
+        backgroundColor: Colors.primary,
+        color: Colors.secondary,
+        padding: 20,
+        borderRadius: 20,
+        gap: 20,
+        borderWidth: 1,
+        borderColor: Colors.border,
+    },
+    titleInput: {
+        backgroundColor: Colors.primary,
+        padding: 10,
+        borderRadius: 20,
+        paddingLeft: 15,
+        borderWidth: 1,
+        borderColor: Colors.border,
+    },
+    descriptionInput: {
+        backgroundColor: Colors.primary,
+        padding: 10,
+        paddingLeft: 15,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: Colors.border,
+        height: 150,
+    },
+};
